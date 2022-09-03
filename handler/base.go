cc7dcdea60eb620acd41191bd85ab22cdc7e8f45
o9ltop <liuzunxiong@qq.com>
2022-09-03 23:44:53 +0800

    123123
    Change-Id: I5f9a10d61b03c7fafd1451ae362e9e7c4f6cbb55
    
    123123
    
    Change-Id: I25ec7b73e28495c538dc727de913ab01bd1fc145

--- handler/base.go
+++ handler/base.go
@@ -1,12 +1,1 @@
 package handler
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-var Methods = map[string]func(r *gin.Engine, api string){
-	"api/get":              RegisterGetData,
-	"api/put":              RegisterPutData,
-	"api/data/account":     RegisterGetAccountData,
-	"api/data/account/set": RegisterSetAccountData,
-}
